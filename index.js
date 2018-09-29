0ca0c7539c8ed55d15a4d71b28c8937bd8631f05
minoj khadgi <minoj_5@hotmail.com>
2018-09-29 03:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = " Neil deGrasse Tyson";
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + `${specialGuest}` + "!";
 
 var topic = "space";
 
 var conversation = "Let\'s talk about "+ `${topic}`+".";
+=======
+var greetSpecialGuest = "Hello, " + `${specialGuest}` +"!";
+
+var topic = "space";
+var conversation = `${topic}`;
+>>>>>>> 38833ba5b53b45b15ee34ed9fc2c51e71cd9ce6c
